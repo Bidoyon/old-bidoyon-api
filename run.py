8680ea8eec29e0e5f433335a7fb8b41f440598e8
Bidulman <63709148+Bidulman@users.noreply.github.com>
2021-10-26 19:08:19 +0200

    Enabled string run

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from uvicorn import run
-from app import app
 
 HOST = "localhost"
 PORT = 8000
 
 if __name__ == "__main__":
-    run(app, host=HOST, port=PORT)
+    run("app:app", host=HOST, port=PORT)
